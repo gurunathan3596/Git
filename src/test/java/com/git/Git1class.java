e7187021325b120376b75d6d742c751f2899d143
gurunathan3596 <gurunathan361@gmail.com>
2024-09-12 11:13:30 +0530

    1st code added

---
+++ src/test/java/com/git/Git1class.java
@@ -0,0 +1,16 @@
+package com.git;
+
+public class Git1class {
+
+	public void test1() {
+		System.out.println(123);
+	}
+	public void test2() {
+		System.out.println(345);
+
+	}
+	public void test3() {
+        System.out.println(678);
+	}
+	
+}
