c3084b4587f9ef6141dc6aa7c92fff56ee7a2761
gurunathan3596 <gurunathan361@gmail.com>
2024-09-13 09:59:04 +0530

    naresh code added

--- src/test/java/com/git/Git1class.java
+++ src/test/java/com/git/Git1class.java
@@ -12,5 +12,15 @@ public class Git1class {
 	public void test3() {
         System.out.println(678);
 	}
+	public void test7() {
+		System.out.println(356);
+	}
+	public void test8() {
+		System.out.println(196);
+
+	}
+	public void test9() {
+        System.out.println(035);
+	}
 	
 }
