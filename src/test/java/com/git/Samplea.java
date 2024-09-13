6d28153d883e8df2c100f2115c0553c2fc23b1ea
gurunathan3596 <gurunathan361@gmail.com>
2024-09-13 09:46:26 +0530

    method add ckcguru

---
+++ src/test/java/com/git/Samplea.java
@@ -0,0 +1,17 @@
+package com.git;
+
+public class Samplea {
+
+
+	public void test1() {
+		System.out.println(123);
+	}
+	public void test2() {
+		System.out.println(345);
+
+	}
+	public void test3() {
+        System.out.println(678);
+	}
+	
+}
